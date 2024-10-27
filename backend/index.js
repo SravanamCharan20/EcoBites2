b7a9fc9a34d203c5187d69b6afdbbafc7f7376f9
charan <sravanamcharan20@gmail.com>
2024-10-27 17:26:36 +0530

    chnages for production

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,12 @@ import DonorForm from './routes/donor.route.js';
 import Donor from '../backend/models/donor.model.js'
 import path from 'path';
 import cors from 'cors';
+
+
 dotenv.config();
 const PORT = 6001;
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use('/uploads', express.static(path.resolve('uploads')));
@@ -17,9 +20,9 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/donor',DonorForm)
 app.use(cors({
-    origin: 'http://localhost:5173', // Allow only this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    credentials: true, // Allow credentials (optional, based on your use case)
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true, 
 }));
 
 app.use((err, req, res, next) => {
@@ -30,6 +33,11 @@ app.use((err, req, res, next) => {
       message,
       statusCode,
   });
+});
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 mongoose.connect(process.env.MONGO).then(()=>{
