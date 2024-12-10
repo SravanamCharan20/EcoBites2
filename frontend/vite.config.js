aeb70a076f7fd2255535ba026f5549c0bc87daea
charan <sravanamcharan20@gmail.com>
2024-12-10 10:25:43 +0530

    c

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:6001',
+        target: 'http://ecobites2-production.up.railway.app',
         secure: false,
       },
     },
