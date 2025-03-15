1a4af5f2658fd76aaa96f9ee451ef160afd32810
charan <sravanamcharan20@gmail.com>
2025-03-16 00:04:39 +0530

    small chnages

--- frontend/src/pages/NonFoodDetails.jsx
+++ frontend/src/pages/NonFoodDetails.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
